3b09bc08bdcb84e223dbb32d67a707001b258c85
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-25 17:45:23 +0600

    uninstall unnecessery dependencies

--- backend/src/api/v1/config/index.js
+++ backend/src/api/v1/config/index.js
@@ -1,18 +1,10 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config({
-  path: path.resolve(__dirname, `../../../../${process.env.NODE_ENV}.env`),
-});
-
-module.exports = {
-  NODE_ENV: process.env.NODE_ENV || 'dev',
-  HOST: process.env.HOST || 'localhost',
-  PORT: process.env.PORT || 3000,
-  dbConnection: () =>
-    mongoose
-      .connect(process.env.CONNECTION_URL)
-      .then(console.log(`DB connection successfull`))
-      .catch((error) => console.log(`Error to connect DB: ${error.message}`)),
+const mongoose = require("mongoose");
+const connectDb = async() => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URL);
+    console.log(`Server Running On ${mongoose.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
 };
+module.exports = connectDb;
--- frontend/src/Page/Signup.jsx
+++ frontend/src/Page/Signup.jsx
@@ -51,53 +51,53 @@ const Signup = (props) => {
     }
     return (
         <Layout>
-        <div className='bg-[#f8f8f8]'>
-            <div className='flex justify-center items-center h-[92vh]'>
-                <div>
-                    <div className=' bg-white p-5'>
-                        <h1 className='text-4xl font-semibold'>Create Your Account</h1>
-                        <div className='grid grid-cols-1 gap-5'>
-                            <div className='flex items-center border'>
-                                <div className='py-2 px-3 border-r'>
-                                    <HiUser className='text-[#679509] text-xl' />
+            <div className='bg-[#f8f8f8]'>
+                <div className='flex justify-center items-center h-[92vh]'>
+                    <div>
+                        <div className=' bg-white p-5'>
+                            <h1 className='text-4xl font-semibold'>Create Your Account</h1>
+                            <div className='grid grid-cols-1 gap-5'>
+                                <div className='flex items-center border'>
+                                    <div className='py-2 px-3 border-r'>
+                                        <HiUser className='text-[#679509] text-xl' />
+                                    </div>
+                                    <input onChange={handleChange} name='firstName' className='py-3 px-3 w-full outline-none' type="text" placeholder='First Name' />
                                 </div>
-                                <input onChange={handleChange} name='firstName' className='py-3 px-3 w-full outline-none' type="text" placeholder='First Name' />
-                            </div>
-                            <div className='flex items-center border'>
-                                <div className='py-2 px-3 border-r'>
-                                    <HiUser className='text-[#679509] text-xl' />
+                                <div className='flex items-center border'>
+                                    <div className='py-2 px-3 border-r'>
+                                        <HiUser className='text-[#679509] text-xl' />
+                                    </div>
+                                    <input onChange={handleChange} name='lastName' className='py-3 px-3 w-full outline-none' type="text" placeholder='Last Name' />
                                 </div>
-                                <input onChange={handleChange} name='lastName' className='py-3 px-3 w-full outline-none' type="text" placeholder='Last Name' />
-                            </div>
-                            <div className='flex items-center border'>
-                                <div className='py-2 px-3 border-r'>
-                                    <MdEmail className='text-[#679509] text-xl' />
+                                <div className='flex items-center border'>
+                                    <div className='py-2 px-3 border-r'>
+                                        <MdEmail className='text-[#679509] text-xl' />
+                                    </div>
+                                    <input onChange={handleChange} name='email' className='py-3 px-3 w-full outline-none' type="text" placeholder='Email Address' />
                                 </div>
-                                <input onChange={handleChange} name='email' className='py-3 px-3 w-full outline-none' type="text" placeholder='Email Address' />
-                            </div>
-                            <div className='flex items-center border'>
-                                <div className='py-2 px-3 border-r'>
-                                    <FaLock className='text-[#679509] text-xl' />
+                                <div className='flex items-center border'>
+                                    <div className='py-2 px-3 border-r'>
+                                        <FaLock className='text-[#679509] text-xl' />
+                                    </div>
+                                    <input onChange={handleChange} name='password' className='py-3 px-3 w-full outline-none' type="text" placeholder='Password' />
                                 </div>
-                                <input onChange={handleChange} name='password' className='py-3 px-3 w-full outline-none' type="text" placeholder='Password' />
-                            </div>
-                            <div className='flex items-center border'>
-                                <div className='py-2 px-3 border-r'>
-                                    <FaLock className='text-[#679509] text-xl' />
+                                <div className='flex items-center border'>
+                                    <div className='py-2 px-3 border-r'>
+                                        <FaLock className='text-[#679509] text-xl' />
+                                    </div>
+                                    <input onChange={handleChange} type="number" name='phone' className='py-3 px-3 w-full outline-none' placeholder='Phone Number' />
                                 </div>
-                                <input onChange={handleChange} type="number" name='phone' className='py-3 px-3 w-full outline-none' placeholder='Phone Number' />
-                            </div>
-                            <div className='flex'>
-                                <button onClick={onSubmit} className='bg-[#679509] text-lg py-3 rounded-full text-white w-full'>Create Account</button>
+                                <div className='flex'>
+                                    <button onClick={onSubmit} className='bg-[#679509] text-lg py-3 rounded-full text-white w-full'>Create Account</button>
+                                </div>
                             </div>
                         </div>
+                        <div className='bg-[#efefef] p-4 text-center'>
+                            <h1>Already have an account? <Link to='/login'>Log In</Link></h1>
+                        </div>
                     </div>
-                    <div className='bg-[#efefef] p-4 text-center'>
-                        <h1>Already have an account? <Link to='/login'>Log In</Link></h1>
-                    </div>
-                </div>
-            </div> 
-        </div>
+                </div> 
+            </div>
         </Layout>
     );
 };
