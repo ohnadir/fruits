959c3592ceb4d5072707a0732d1379ef2dc90846
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-25 17:52:34 +0600

    change structure

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,39 @@
-const app = require('./app');
-const { PORT, HOST } = require('./src/api/v1/config');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require("dotenv");
+const app = express();
+const connectDb = require("./src/api/v1/config");
+const errorMiddleware = require('./src/api/v1/middleware/errors')
+
+// config dot env file and connected mongodb
+dotenv.config();
+connectDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(errorMiddleware)
+
+// all route
+const productRoute = require("./src/api/v1/routes/products");
+const userRoute = require("./src/api/v1/routes/users");  
+const paymentRoute = require("./src/api/v1/routes/payment");
+
+// user route 
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/places", productRoute);
+app.use("/api/v1/payments", paymentRoute);
+
+
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+app.get('/', (req, res) => {
+  res.send('airbnb Testing connection')
+})
 
 app.listen(PORT, HOST, () => {
   console.log(`Server started on ${HOST}:${PORT}, url http://${HOST}:${PORT}`);
