481f68f97ce6903010e014e8f2d05fc4b2709dda
Nadir Hossain <nadirhossain336@gmail.com>
2023-01-25 17:55:30 +0600

    something change on product model

--- backend/src/api/v1/models/index.js
+++ backend/src/api/v1/models/index.js
@@ -2,11 +2,9 @@ const User = require('../user/Model');
 const Product = require('../product/Model');
 const Category = require('../category/Model');
 const Order = require('../order/Model');
-const Photo = require('../photoUploader/Model');
 module.exports = {
   User,
   Product,
   Category,
   Order,
-  Photo
 };
