d12534aae478d1a065cf575f5d83fc8bc37da0e6
Nadir Hossain <nadirhossain336@gmail.com>
2023-05-24 15:08:31 +0600

    login and signup page modify

--- frontend/src/Page/Login.jsx
+++ frontend/src/Page/Login.jsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { FaUser } from 'react-icons/fa';
-import { FaLock } from 'react-icons/fa';
-import { login, clearErrors } from '../Redux/actions/user'
+import { BsEyeSlash } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+import { GrFacebookOption } from 'react-icons/gr';
+import { FcGoogle } from 'react-icons/fc';
+import { login} from '../Redux/actions/user'
 import { useDispatch, useSelector } from 'react-redux';
 import {  message } from 'antd';
-import Layout from '../Component/Layout/Layout';
 import { useNavigate } from 'react-router-dom';
 import "../Style/Authentication.scss"
 
 const Login = () => {
     const [messageApi, contextHolder ] = message.useMessage();
     const [auth, setAuth] = useState('');
-    const [borderColor, setBorderColor] = useState("")
+    const [password, setPassword] = useState(false)
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { isAuthenticated, messages } = useSelector(state => state.auth);
@@ -37,35 +38,74 @@ const Login = () => {
         }
     }
     return (
-        <Layout>
             <>
             {contextHolder}
             <div className='login'>
                 <div className='flex justify-center items-center h-[91.5vh]'>
                     <div className='login-container bg-white'>
                         <div className='py-5'>
-                            <h1 className='text-lg font-semibold text-center px-10'>Log In To Your Account</h1>
-                            <div className='grid grid-cols-1 gap-5 px-10'>
-                                <div className='input-container' style={{border: borderColor === "userName" ? "1px solid #679509" : null }}  onClick={()=>setBorderColor("userName")}> 
-                                    <label htmlFor=""><FaUser /></label>
-                                    <input onChange={handleChange} name='user_name' className='' type="text" placeholder='User Name' />
-                                </div>
-                                <div className='input-container' style={{border: borderColor === "password" ? "1px solid #679509" : null }}  onClick={()=>setBorderColor("password")}>
-                                    <label htmlFor=""><FaLock /></label>
-                                    <input onChange={handleChange} name='password' className='' type="password" placeholder='Password' />
-                                </div>
-                                <button onClick={onSubmit}>Login </button>
+                            <div className='mb-8'>
+                                <h1 className='text-[30px] font-bold text-center px-10 m-0'>Login</h1>
+                                <h3 className='m-0 text-[#6b7280] text-center text-[16px]'>Login with your email and password</h3>
                             </div>
-                        </div>
-                        <div className='bg-[#efefef] text-center py-4'>
-                            <h1 className='m-0 text-[12px] text-[#4d4d4d]'>Don't have an account? <span className='text-[#0d6efd] cursor-pointer' onClick={()=>navigate("/signup")}>Sign up</span></h1>
-                            <h1 className='m-0 text-[12px] text-[#4d4d4d]'>Loss Your Password ! <span className='text-[#0d6efd] cursor-pointer'>Forgot Password?</span> </h1>
+                            <div className='grid grid-cols-1 gap-5 px-10'>
+                                <div>
+                                    <p className='label-text'>Email</p>
+                                    <div className='input-container'> 
+                                        <label className='absolute left-3 top-[13px]  z-10' htmlFor="">
+                                            <svg stroke="#808080" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                                                <polyline points="22,6 12,13 2,6"></polyline>
+                                            </svg>
+                                        </label>
+                                        <input onChange={handleChange} inputMode='email' name='email' type="text" placeholder='Email' required />
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='label-text'>Password</p>
+                                    <div className='input-container'>
+                                        <label htmlFor="">
+                                            <svg stroke="#808080" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                                            </svg>
+                                        </label>
+                                        <input onChange={handleChange} name='password' type={password ? "text" :  "password"} placeholder='Password' required />
+                                        {
+                                            auth.password
+                                            ? 
+                                            <span onClick={()=>setPassword(!password)} className='show-password-btn'>
+                                                <BsEyeSlash style={{color : "#808080"}} size={15} />
+                                            </span> 
+                                            :
+                                            null
+                                        }
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='m-0 underline'> Forgot password</p>
+                                </div>
+                                <button className='login-btn' onClick={onSubmit}>Login</button>
+                            </div>
+                            <div className='login-divider'>OR</div>
+                            <div className='social-auth px-10 grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                                <button className='social-btn'>
+                                    <GrFacebookOption size={19} style={{backgroundColor : "transparent"}} />
+                                    <span>Login with Facebook</span>
+                                </button>
+                                <button className='social-btn google-btn'>
+                                    <FcGoogle size={19} style={{backgroundColor : "transparent"}} />
+                                    <span>Sign in with Google</span>
+                                </button>
+                                
+                            </div>
+                            <div className='text-[15px] text-center mt-3'>
+                                <span className='text-[#6b7280] '>Not have an account ? <span className='text-black font-bold'>Register</span></span>
+                            </div>
                         </div>
                     </div>
                 </div> 
             </div>
             </>
-        </Layout>
     );
 };
 
--- frontend/src/Page/Signup.jsx
+++ frontend/src/Page/Signup.jsx
@@ -5,13 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { register, clearErrors } from '../Redux/actions/user'
 import {  message } from 'antd';
 import { useNavigate } from 'react-router-dom'
-import Layout from '../Component/Layout/Layout';
 import "../Style/Authentication.scss"
+import { GrFacebookOption } from 'react-icons/gr';
+import { FcGoogle } from 'react-icons/fc';
+import { BsEyeSlash } from 'react-icons/bs';
 
 const Signup = () => {
     const [messageApi, contextHolder ] = message.useMessage();
     const [auth, setAuth] = useState('');
-    const [borderColor, setBorderColor] = useState("")
+    const [password, setPassword] = useState(false)
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { isAuthenticated, messages  } = useSelector(state => state.auth);
@@ -36,42 +38,85 @@ const Signup = () => {
         }
     }
     return (
-        <Layout>
             <>
             {contextHolder}
-            <div className='registration '>
-                <div className='flex justify-center items-center h-[92vh]'>
-                    <div className='registration-container '>
-                        <div className=' bg-white p-5'>
-                            <h1 className='text-[16px] font-semibold text-center'>Create Your Account</h1>
-                            <div className='grid grid-cols-1 gap-5'>
-                                <div style={{border: borderColor === "userName" ? "1px solid #679509" : null }} className='input-container' onClick={()=>setBorderColor("userName")}>
-                                    <label htmlFor=""><FaUser /></label>
-                                    <input onChange={handleChange} name='user_name' className='' type="text" placeholder='User Name' />
-                                </div>
-                                <div style={{border: borderColor === "email" ? "1px solid #679509" : null }}  onClick={()=>setBorderColor("email")} className='input-container'>
-                                    <label htmlFor=""><MdEmail /></label>
-                                    <input onChange={handleChange} name='email' className='' type="email" placeholder='Email Address' />
-                                </div>
-                                <div className='input-container' style={{border: borderColor === "password" ? "1px solid #679509" : null }}  onClick={()=>setBorderColor("password")}>
-                                    <label htmlFor=""><FaLock  /></label>
-                                    <input onChange={handleChange}  name='password' type="password" placeholder='Password' />
-                                </div>
-                                <div className='input-container' style={{border: borderColor === "confirm-pass" ? "1px solid #679509" : null }}  onClick={()=>setBorderColor("confirm-pass")}>
-                                    <label htmlFor=""><FaLock /></label>
-                                    <input onChange={handleChange} type="password" name='confirm_pass'  placeholder='Confirm Password' />
-                                </div>
-                                <button onClick={onSubmit}>Create Account</button>
+            <div className='login'>
+                <div className='flex justify-center items-center h-[91.5vh]'>
+                    <div className='login-container bg-white'>
+                        <div className='py-5'>
+                            <div className='mb-8'>
+                                <h1 className='text-[30px] font-bold text-center px-10 m-0'>Signing Up</h1>
+                                <h3 className='m-0 text-[#6b7280] text-center text-[16px]'>Create an account with email</h3>
                             </div>
-                        </div>
-                        <div className='bg-[#efefef] p-4 text-center'>
-                            <h1 className='m-0 text-[12px] text-[#4d4d4d]'>Already have an account? <span className='text-[#0d6efd] cursor-pointer' onClick={()=>navigate("/login")}>Login</span></h1>
+                            <div className='grid grid-cols-1 gap-5 px-10'>
+                                <div>
+                                    <p className='label-text'>Name</p>
+                                    <div className='input-container'> 
+                                        <label htmlFor="">
+                                        <svg stroke="#808080" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle>
+                                        </svg>
+                                        </label>
+                                        <input onChange={handleChange}  name='name' type="text" placeholder='Full Name' required />
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='label-text'>Email</p>
+                                    <div className='input-container'> 
+                                        <label className='absolute left-3 top-[13px]  z-10' htmlFor="">
+                                            <svg stroke="#808080" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                                                <polyline points="22,6 12,13 2,6"></polyline>
+                                            </svg>
+                                        </label>
+                                        <input onChange={handleChange} inputMode='email' name='email' type="text" placeholder='Email' required />
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='label-text'>Password</p>
+                                    <div className='input-container'>
+                                        <label htmlFor="">
+                                            <svg stroke="#808080" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                                                <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                                            </svg>
+                                        </label>
+                                        <input onChange={handleChange} name='password' type={password ? "text" :  "password"} placeholder='Password' required />
+                                        {
+                                            auth.password
+                                            ? 
+                                            <span onClick={()=>setPassword(!password)} className='show-password-btn'>
+                                                <BsEyeSlash style={{color : "#808080"}} size={15} />
+                                            </span> 
+                                            :
+                                            null
+                                        }
+                                    </div>
+                                </div>
+                                <div>
+                                    <p className='m-0 underline'> Forgot password</p>
+                                </div>
+                                <button className='login-btn' onClick={onSubmit}>Register</button>
+                            </div>
+                            <div className='login-divider'>OR</div>
+                            <div className='social-auth px-10 grid grid-cols-1 lg:grid-cols-2 gap-4'>
+                                <button className='social-btn'>
+                                    <GrFacebookOption size={19} style={{backgroundColor : "transparent"}} />
+                                    <span>Login with Facebook</span>
+                                </button>
+                                <button className='social-btn google-btn'>
+                                    <FcGoogle size={19} style={{backgroundColor : "transparent"}} />
+                                    <span>Sign in with Google</span>
+                                </button>
+                                
+                            </div>
+                            <div className='text-[15px] text-center mt-3'>
+                                <span className='text-[#6b7280] '>Already have an account ? <span className='text-black font-bold'>Login</span></span>
+                            </div>
                         </div>
                     </div>
                 </div> 
             </div>
             </>
-        </Layout>
     );
 };
 
