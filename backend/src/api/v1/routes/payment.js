4938baa9131644c97330b76357bbb1cd2f93e1e2
Nadir Hossain <nadirhossain336@gmail.com>
2023-06-10 15:34:33 +0600

    created stripe client api key

--- backend/src/api/v1/routes/payment.js
+++ backend/src/api/v1/routes/payment.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { processPayment } = require('../payment/controller')
+const { processPayment, paymentApi } = require('../payment/controller')
 
 router.post('/process',  processPayment);
+router.get('/api',  paymentApi);
 module.exports = router;
